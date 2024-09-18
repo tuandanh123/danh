9ff76b8782f38acaa4262a6b0e691498b0a3defa
tuandanh123 <145439094+tuandanh123@users.noreply.github.com>
2024-09-18 10:55:18 +0700

    Update User.java

--- Desktop/learningeveryday/src/main/java/com/example/learningeveryday/entity/User.java
+++ Desktop/learningeveryday/src/main/java/com/example/learningeveryday/entity/User.java
@@ -14,6 +14,7 @@ public class User {
     private String acd;
     private String cada;
     private String chuso;
+    private String tuandanh;
     private String password;
     private String username;
     private String firstname;
