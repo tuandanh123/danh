521820e3aa039272c1945343714b159a035f1026
tuandanh123 <145439094+tuandanh123@users.noreply.github.com>
2024-09-18 11:04:14 +0700

    Update User.java

--- Desktop/learningeveryday/src/main/java/com/example/learningeveryday/entity/User.java
+++ Desktop/learningeveryday/src/main/java/com/example/learningeveryday/entity/User.java
@@ -10,8 +10,6 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
-    private String tuandanh;
-    private int chuso;
     private String password;
     private String username;
     private String firstname;
